8c0c3478bee80175b6f3b6dd00e1d00ddf946a8f
vojtech <p.prusavojta@gmail.com>
2022-11-10 22:59:00 +0100

    Final tweaks

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 def show_board():
     print(f"{board[1]} | {board[2]} | {board[3]}")
-    print("--------- ")
+    print("---------")
     print(f"{board[4]} | {board[5]} | {board[6]}")
     print("---------")
     print(f"{board[7]} | {board[8]} | {board[9]}\n")
